84d5c69e16269bd09dd15d14f9c303a9b3efa2c8
kokorii <kokori91@gmail.com>
2023-04-14 17:54:34 +0900

    야곰 유튜브 iOS 개발 입문 강의 실습

---
+++ CreditViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by kokori on 2023/04/14.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpDismissButton( sender:UIButton){
+        dismiss(animated: true,
+                 completion: nil)
+    }
+
+}
